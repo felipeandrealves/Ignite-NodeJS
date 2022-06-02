29a018d8586da73c14d3c9393fd99d18df2e7456
Felipe Andre Alves <felipe_alves_sbo@hotmail.com>
2022-06-02 18:51:08 -0300

    add: 'Listando extrato bancário por data'

--- chapter-01/finapi/src/index.js
+++ chapter-01/finapi/src/index.js
@@ -56,6 +56,21 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer.statement);
 });
 
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (operation) =>
+      operation.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.json(customer.statement);
+});
+
 app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
   const { description, amount } = request.body;
 
