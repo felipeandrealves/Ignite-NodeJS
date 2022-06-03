372b7497a52f8c8e726ccabb56ee65dc5aee5279
Felipe Andre Alves <felipe_alves_sbo@hotmail.com>
2022-06-03 15:45:43 -0300

    rentalx: Separando os repositórios

--- chapter-02/rentalx/src/routes/categories.routes.ts
+++ chapter-02/rentalx/src/routes/categories.routes.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategory';
 
 const categoriesRoutes = Router();
-
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request, response));
 
